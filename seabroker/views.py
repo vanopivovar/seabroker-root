3e8ec4dca2eb4dbe1cbe1405df174f5b446267cb
vanopivovar <vanopivovar@gmail.com>
2022-09-20 00:02:07 +0200

    Add reverse text

--- seabroker/views.py
+++ seabroker/views.py
@@ -6,3 +6,8 @@ def about(request):
 
 def home(request):
     return render(request, 'home.html', {'greeting':'Hello!'})
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text [::-1]
+    return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text})
